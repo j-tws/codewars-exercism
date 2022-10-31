7cce9b0e3550afa07de4215e51a9a8decd49845e
Justin Tan <justintws10@gmail.com>
2022-10-31 22:51:40 +1100

    started on oop & testing exercise

---
+++ oddint.rb
@@ -0,0 +1,30 @@
+# require 'pry'
+
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+# Examples
+
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+  
+def find_it seq
+
+  seq.each do |el| 
+
+    el_count = seq.count el
+    if el_count % 2 == 1
+      return el
+    end
+
+  end
+
+end
+
+p find_it [0,1,0,1,0]
+
+# binding.pry
---
+++ oop-testing-exercise/main.js
@@ -0,0 +1,89 @@
+import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import { setupCounter } from './counter.js'
+
+// # Bank account 
+// Skills tested - Object oriented programming & test driven development
+
+// - Create a Bank & BankAccount class.
+// - A Bank will have multiple Bank accounts.
+// - A bank account will have the following fields:
+//     - Account Number
+//     - Balance
+//     - Customer Name
+// - You must be able to retrieve the value of all fields (OOP - Getter), but only `Balance` and `Customer Name` values can be updated (OOP - Setter).
+// - Create methods allow deposit/withdrawal of funds. Ensure withdrawals will fail if insufficient funds. 
+//     - You should respond with an object indicating transaction success / failure, and the new balance
+// - Create method that allows changing the account's name
+// - Write tests to confirm that invalid inputs are blocked, and any refactoring does not break existing functionalities
+//     - Block negative value deposits
+//     - Block withdrawals over limits
+//     - Account Number must be unique
+
+
+console.log('exercise loaded')
+
+// Create class Bank as parent class 
+class Bank {
+  constructor(customerName, balance) {
+    this._customerName = customerName,
+    this._balance = balance,
+    this._accountNum = `BANKACCNO-${Math.floor(Math.random() * 10000)}`
+  }
+
+  get customerName(){
+    return this._customerName
+  }
+
+  get balance(){
+    return this._balance
+  }
+
+  get accountNum(){
+    return this._accountNum
+  }
+
+
+}
+
+// class BankAccount as child class of Bank class
+class BankAccount extends Bank {
+  constructor(customerName, balance) {
+    super(customerName, balance);
+  }
+
+  withdraw(amount){
+    
+    if (this._balance < amount){
+      return `You are withdrawing $${amount} from your account with $${this._balance}. You got no money`
+    }
+
+    this._balance -= amount
+    return `You withdrew $${amount}, your balance is now $${this._balance}`
+
+  }
+
+  deposit(amount){
+    this._balance += amount
+  
+  }
+
+  changeName(newName){
+    this._customerName = newName
+  }
+
+}
+
+const testAcc = new BankAccount('John', 5000)
+
+console.log(testAcc.customerName)
+console.log(testAcc.balance)
+
+testAcc.deposit(500)
+console.log(testAcc.withdraw(2000))
+console.log(testAcc.withdraw(5000))
+// testAcc.withdraw(1000)
+// testAcc.withdraw(10000)
+// console.log(testAcc.balance)
+
+
